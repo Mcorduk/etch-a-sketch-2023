119ac25c9907d1d66966da9d518b1ff350fa3c07
Mcorduk <om.corduk@gmail.com>
2023-11-26 16:23:14 +0300

    Finished

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function addGrid(counter) {
     const container = document.querySelector("div");
-    while (counter > 0) {
+    const size = counter ** 2;
+    for (let i = 0; i < size; i++) {
         const grid = document.createElement("div");
-        grid.classList.add("grid");
+        grid.classList.add("grid")
+        grid.style.border = "1px solid black"
+        grid.style.height = `${800 / counter}px`
+        grid.style.width = `${800 / counter}px`
         container.appendChild(grid);
-        counter--;
     }
+    setColorOnHover()
 };
 
 
@@ -27,7 +31,7 @@ function getUserInput() {
         ("Pick a grid size! For instance '8' for an 8x8 grid."));
 
     while (isNaN(userInput) || typeof userInput !== 'number' ||
-     userInput > 100 || userInput === undefined) {
+        userInput > 100 || userInput === undefined || userInput <=0) {
         userInput = Number(prompt
             (`Invalid input. Pick a grid size! For instance '8' for an 8x8 grid.
 Only numbers up to 100 are accepted.`));
@@ -35,21 +39,22 @@ Only numbers up to 100 are accepted.`));
     return userInput;
 }
 
+function setColorOnHover() {
+    // Get all elements with the class "grid"
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach(grid => {
+        grid.addEventListener('mouseover', function () {
+            // Set the background color when the mouse is over the element
+            let rgb = getRandomColor();
+            let red, green, blue;
+            red = rgb[0];
+            green = rgb[1];
+            blue = rgb[2];
 
-// Get all elements with the class "grid"
-const grids = document.querySelectorAll(".grid");
-grids.forEach(grid => {
-    grid.addEventListener('mouseover', function () {
-        // Set the background color when the mouse is over the element
-        let rgb = getRandomColor();
-        let red, green, blue;
-        red = rgb[0];
-        green = rgb[1];
-        blue = rgb[2];
-
-        grid.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            grid.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+        });
     });
-});
+}
 
 const button = document.querySelector("button");
 button.addEventListener("click", function () {
@@ -58,4 +63,6 @@ button.addEventListener("click", function () {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+    addGrid(userInput);
+    setColorOnHover();
 });
