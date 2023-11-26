5431aa7ab0bcddb8e85fc29ade4539c6c8d52571
Mcorduk <om.corduk@gmail.com>
2023-11-26 15:41:07 +0300

    Add Random Color on Grid Hover Over

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function addGrid(counter) {
     const container = document.querySelector("div");
-
     while (counter > 0) {
         const grid = document.createElement("div");
         grid.classList.add("grid");
@@ -10,3 +9,32 @@ function addGrid(counter) {
 }
 
 addGrid(16);
+
+function getRandomColor() {
+    const colorRange = 255;
+    let rgb = [0, 0, 0];
+    for (let i = 0; i < rgb.length; i++) {
+        let randomColor =
+            Math.round(Math.random() * colorRange);
+        rgb[i] = randomColor;
+    }
+    return rgb
+}
+
+
+// Get all elements with the class "grid"
+const grids = document.querySelectorAll(".grid");
+grids.forEach(grid => {
+    grid.addEventListener('mouseover', function () {
+        // Set the background color when the mouse is over the element
+        let rgb = getRandomColor();
+        let red, green, blue;
+        red = rgb[0];
+        green = rgb[1];
+        blue = rgb[2];
+
+        grid.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+    });
+});
+
+
